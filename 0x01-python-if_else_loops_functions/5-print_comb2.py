a5377eee1a0df947340e4203c6da12b9cccadcd5
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-10-04 02:27:19 +0100

    added 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 for i in range(0,100):
-    if i < 10:
-       i = ("{:02},".format(i))
-    elif i != 99:
-       i = ("{:d},".format(i))
-    elif i == 99:
-       i = ("{:d}\n").format(i)
-
-    if i != 99:
-        print(i, end=" ")
-    elif i == 99:
-        print(i)
+    if n < 99:
+        print("{:02d}".format(n), end=", ")
+    else:
+        print("{:d}".format(n))
