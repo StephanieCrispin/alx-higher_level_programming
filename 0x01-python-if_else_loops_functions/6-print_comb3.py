d8ef0049ea9a0497724263600398838e0daa9e41
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-10-04 22:28:26 +0100

    added 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for k in range(0, 10):
+    for w in range(0, 10):
+        if k > w:
+            continue
+        if w == 9 and k > 8:
+            print("{:d}{:d}".format(k, w))
+        else:
+            print("{:d}{:d}".format(k, w), end=", ")
