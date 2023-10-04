9d4d69f8adb141803aeb0832405b73b03a0dc222
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-10-04 22:33:17 +0100

    added 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-for k in range(0, 10):
+for k in range(0, 9):
     for w in range(0, 10):
         if k > w:
             continue
-        if w == 9 and k > 8:
+        if w == 9 and k == 8:
             print("{:d}{:d}".format(k, w))
+            break
         else:
             print("{:d}{:d}".format(k, w), end=", ")
