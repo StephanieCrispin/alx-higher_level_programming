d2dd2806b4a064681c8847c3fe82aa2a306973f2
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-10-04 02:18:13 +0100

    made updates

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 for i in range(0,100):
     if i < 10:
-       i = ("{:02}, ".format(i))
+       i = ("{:02},".format(i))
     elif i != 99:
-       i = ("{:d}, ".format(i))
+       i = ("{:d},".format(i))
     elif i == 99:
        i = ("{:d}\n").format(i)
 
