dc9eaed7dafd620959e088e6c36f6fd7ed33d6da
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-10-05 05:31:11 +0100

    added 13

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+listint_t *createNewNode(listint_t *newNode, int number)
+{
+    newNode = malloc(sizeof(listint_t));
+    if (!newNode)
+        return NULL;
+
+    newNode->n = number;
+    newNode->next = NULL;
+    return (newNode);
+}
+
+listint_t *insert_node(listint_t **head, int number)
+{
+    listint_t *newNode;
+    listint_t *currentNode;
+    listint_t *MynewNode = createNewNode(newNode, number);
+    /*If node is the smalles number set it to the first node*/
+    if (*head == NULL || (number < (*head)->n))
+    {
+        newNode->next = *head;
+        *head = MynewNode;
+        return (MynewNode);
+    }
+
+    while (currentNode->next != NULL && currentNode->next->n > MynewNode->n)
+        return (currentNode->next);
+
+    MynewNode->next = currentNode->next;
+    currentNode->next = MynewNode;
+
+    return (currentNode);
+}
