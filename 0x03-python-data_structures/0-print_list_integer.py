7495eebfb262828aa739675e4dd8bc12f87dabaa
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-10-09 00:53:27 +0100

    changed the logic

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,4 +2,5 @@
 """Prints all integers of a list"""
 if __name__ == "__main__":
     def print_list_integer(my_list=[]):
-        [print("{:d}".format(item)) for item in my_list]
+        for item in my_list:
+            print("{:d}".format(item))
