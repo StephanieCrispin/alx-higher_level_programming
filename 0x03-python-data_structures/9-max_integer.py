10d61fd80bdf7354d103d64180964d7bf7b688ae
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-10-09 03:28:26 +0100

    added 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,9 +3,9 @@ def max_integer(my_list=[]):
 
     if my_list == []:
         return None
-    elif isinstance(my_list, list):
-        count = 0
-        for i in my_list:
-            if int(i) > count:
-                count = int(i)
-        return count
+
+    count = 0
+    for i in my_list[:len(my_list)]:
+        if int(i) > count:
+            count = int(i)
+    return count
