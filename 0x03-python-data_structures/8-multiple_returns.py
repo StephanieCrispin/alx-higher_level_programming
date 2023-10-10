093ed5588a087fe52c8c47ff7cd10cbbf5e4f388
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-10-10 01:48:24 +0100

    Made updates

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,13 +2,7 @@
 
 def multiple_returns(sentence):
     length = len(sentence)
-    char = sentence[0]
     if not length:
         sentence[0] = None
-    newtupple = (length, char)
-    return (newtupple)
-
-
-sentence = "At school, I learnt C!"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
+    char = sentence[0]
+    return (length, char)
