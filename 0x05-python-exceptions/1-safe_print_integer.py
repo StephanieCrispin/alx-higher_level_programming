96366b732cf39117e57b2a69e75b926d7de9adf6
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-10-24 01:07:42 +0100

    added 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    # try:
-    print("{:d}".format(value))
-    return (True)
-
-
-
-safe_print_integer(55)
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except ValueError:
+        return (False)
+    
