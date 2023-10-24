5b34bdff2003abb55fd796c0f54e94acf6195dc6
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-10-24 21:53:14 +0100

    created task 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+class Square:
+    """A class that creates a square object
+
+        Attributes:
+        size(int): The size of the square
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a class object.
+
+        parameters: 
+            size(int): The size of the square
+        """
+        self.__size = size
