50eeda5cf971db81ac7cada6c577920440efd1f4
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-11-08 00:13:06 +0100

    worked on input output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""This file defines a function that reads from a text file"""
+"""Defines a function that reads from a text file"""
 
 
 def read_file(filename=""):
-    """ Reads from a text file """
-    with open(filename, "r", encoding="utf-8") as f:
-        print(f.read, end="")
+    """Function that reads from my text file and prints it to stdout"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
