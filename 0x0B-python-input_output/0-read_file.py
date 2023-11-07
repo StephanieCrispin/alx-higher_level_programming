fbf34e5beda0d60da0d42645ac6089ed31854216
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-11-08 00:10:05 +0100

    worked on input output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,5 @@
 
 def read_file(filename=""):
     """ Reads from a text file """
-    with open(filename, encoding="utf-8") as f:
-        read = f.read()
-        print(read, end="")
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read, end="")
