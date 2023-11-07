5288c2edae14abdc646a9c6421c6e09280d6246b
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-11-08 00:03:41 +0100

    worked on input output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+
+"""A program that writes to a file"""
+
+
 def write_file(filename="", text=""):
     """A function that writes to a file"""
     with open(filename, "w", encoding="utf-8") as f:
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+"""A program that appends to a file"""
+
+
 def append_write(filename="", text=""):
     """An object that appends a string to a text file"""
     with open(filename, "a+", encoding="utf-8") as f:
