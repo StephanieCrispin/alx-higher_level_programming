1d745b652f0c3a46959ef835c9823511cc06287d
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-12-14 02:48:08 +0100

    corectons

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- A script that creates a table
-CREATE TABLE IF NOT EXIST  `first_table` ( `id` int, `name` varchar(256));
+CREATE TABLE IF NOT EXISTS `first_table` ( `id` int, `name` varchar(256));
