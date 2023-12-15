755a03897d7c4b812c1bfbc2ebf01e5cf18b3193
Stephanie Okpomfon <Stephanie Okpo-Mfon>
2023-12-15 16:47:42 +0100

    correction

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- A task that lists the permissions of a user
-SHOW GRANTS FOR 'user_0d_1'@'host';
-SHOW GRANTS FOR 'user_0d_2'@'host';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
