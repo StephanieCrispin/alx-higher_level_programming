a6134bd1a35149d1e72016ef306e7ce413279685
Stephanie Okpomfon <Stephanie Okpo-Mfon>
2023-12-15 18:19:47 +0100

    9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- A script that uses a join method
-SELECT `cities`.`id`, `cities`.`name`,`states`.`order_id` FROM `cities` INNER JOIN `states` ON `cities`.`state_id`= `state`.`id` ORDER BY `cities`.`id`
+SELECT `cities`.`id`, `cities`.`name`,`states`.`name` FROM `cities` INNER JOIN `states` ON `cities`.`state_id`= `states`.`id` ORDER BY `cities`.`id`
