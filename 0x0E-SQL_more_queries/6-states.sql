16e6b153adee0bc6c8fae21af4ae6a4b384d164f
Stephanie Okpomfon <Stephanie Okpo-Mfon>
2023-12-15 17:53:38 +0100

    correrction

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,3 @@
 -- A Script that creates a database and a table
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `hbtn_0d_use`.`states` (`id` INT NOT NULL AUTO_INCREMENT, UNIQUE(`id`), PRIMARY KEY (`id`))
+CREATE TABLE IF NOT EXISTS `hbtn_0d_use`.`states` (`id` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR(256), UNIQUE(`id`), PRIMARY KEY (`id`))
