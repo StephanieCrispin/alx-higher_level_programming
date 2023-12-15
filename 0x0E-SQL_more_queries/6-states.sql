8333caf15e080472a1e541db2f4bcdd1a459f4b9
Stephanie Okpomfon <Stephanie Okpo-Mfon>
2023-12-15 17:45:25 +0100

    task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,3 @@
+-- A Script that creates a database and a table
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_use`.`states` (`id` INT NOT NULL AUTO_INCREMENT, UNIQUE(`id`), PRIMARY KEY (`id`))
