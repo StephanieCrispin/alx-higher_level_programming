1da71caf8915a3efbe18a6a5241bc6b69fe5e473
Stephanie Okpomfon <Stephanie Okpo-Mfon>
2023-12-16 13:44:12 +0100

    correction

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -4,7 +4,7 @@ SELECT t.`title`
     INNER JOIN `tv_shows_genres` AS s
     ON t.`id` = s.`show_id`
 
-    INNER JOIN `tv_genres` as g
+    INNER JOIN `tv_genres` AS g
     ON g.`id` = s.`genre_id`
     WHERE g.`name` = 'Comedy'
 ORDER BY t.`title`;
