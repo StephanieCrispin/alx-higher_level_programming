a14842b6edb36d90ca599f9c2c4e2df30b0801f3
Stephanie Okpomfon <Stephanie Okpo-Mfon>
2023-12-16 12:41:52 +0100

    format

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,7 @@
 -- Lists all shows in hbtn_02_shows database
 -- Returns null if a show has not yet been assigned a genre
-SELECT s.`title`, g.`genre_id` from `tv_shows` AS s
-    LEFT JOIN `tv_show_genres` as g ON s.`id`=g.`show_id`
-    ORDER BY s.title, g.genre_id;
-    
+SELECT s.`title`, g.`genre_id`
+ FROM `tv_shows` AS s
+    LEFT JOIN `tv_show_genres` AS g
+    ON s.`id`=g.`show_id`
+ORDER BY s.`title`, g.`genre_id`;
