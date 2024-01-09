8480e0d63e7bd2bd3db147b33dcc8f23aec4def8
StephanieMfon <okpomfonstephanie@gmail.com>
2024-01-09 02:03:41 +0100

    task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,9 +3,9 @@
 
 let i;
 if (isNaN(process.argv[2])) {
-  console.log("Missing number of occurences");
+  console.log('Missing number of occurences');
 } else {
   for (i = 0; i < Number(process.argv[2]); i++) {
-    console.log("C is fun");
+    console.log('C is fun');
   }
 }
