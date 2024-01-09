58a397878c3b53574dd8495020a9b062530cab69
StephanieMfon <okpomfonstephanie@gmail.com>
2024-01-09 01:08:41 +0100

    used semistandard

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,6 +2,6 @@
 // A script that works with command line arguments
 
 const commands = process.argv.length;
-if (commands === 2) console.log("No argument");
-else if (process.argv.length === 3) console.log("Argument found");
-else console.log("Arguments found");
+if (commands === 2) console.log('No argument');
+else if (process.argv.length === 3) console.log('Argument found');
+else console.log('Arguments found');
