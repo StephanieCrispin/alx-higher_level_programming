30cbd422906b789337b5d55866968c1ef30f3c14
StephanieMfon <okpomfonstephanie@gmail.com>
2024-01-09 01:01:08 +0100

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,6 +2,6 @@
 // A script that works with command line arguments
 
 const commands = process.argv.length;
-if (commands < 0) console.log("No argument");
-else if (process.argv.length === 1) console.log("Argument found");
+if (commands === 2) console.log("No argument");
+else if (process.argv.length === 3) console.log("Argument found");
 else console.log("Arguments found");
