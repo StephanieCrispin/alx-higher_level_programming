f8e9da13761686ef5e12df0bc02f041b617f9b7f
StephanieMfon <okpomfonstephanie@gmail.com>
2024-01-09 01:53:27 +0100

    added 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Loops through and prints out array items
+
+const array = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
